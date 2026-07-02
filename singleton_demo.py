4583515422a33b81f90de6088256260a052020f6
Omkar.Devarmani <gdomkar@gmail.com>
2026-07-02 11:28:28 +0530

    Update singleton_demo.py

--- singleton_demo.py
+++ singleton_demo.py
@@ -1,1 +1,16 @@
+class DatabaseConnection:
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            print("Creating database connection...")
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    def connect(self):
+        print("Connected to the database")
 
+
+db1 = DatabaseConnection()
+db1.connect()
+db2 = DatabaseConnection()
+db2.connect()
+print(db1 is db2)
